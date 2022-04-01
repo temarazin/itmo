d242fcaf75e036c534878dccc5c58533617bedac
Masha Murochkina <a.axylu@gmail.com>
2022-04-01 13:31:45 +0200

    fix: changed images in cards

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, './src'),
     compress: true,
     port: 8080,
 
