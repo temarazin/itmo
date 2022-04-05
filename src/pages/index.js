5ffe676a6e87acdd17bd2f2e6e12a33e928a9f19
Lekozhe <v.a.kozh@ya.ru>
2022-04-05 16:01:15 +0300

    feat: open education

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import './index.css';
 import {
   menu,
-  buttonMenu
+  menuItems,
+  buttonMenu,
+  buttonClose
 } from '../pages/scripts/utils/constants'
 
 
-buttonMenu.addEventListener('click', toggleMenu);
+buttonMenu.addEventListener('click', openMenu);
+buttonClose.addEventListener('click', closeMenu);
+menuItems.addEventListener('click', openEducation)
 
-function toggleMenu() {
-  menu.classList.toggle('menu_open');
-  console.log(menu)
+function openMenu() {
+  menu.classList.add('menu_open');
+  buttonMenu.style.display = 'none';
+}
+
+function closeMenu() {
+  menu.classList.remove('menu_open');
+  buttonMenu.style.display = 'flex';
+}
+
+function openEducation(e) {
+  if (e.target.closest('.menu__link_type_study'))
+    document.querySelector('.menu__sublist').classList.toggle('menu__sublist_open')
 }
