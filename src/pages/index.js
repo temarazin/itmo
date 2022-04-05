5b572f113a5e0ba0d48a11ff96858b8aea7f42f0
Lekozhe <v.a.kozh@ya.ru>
2022-04-05 16:17:52 +0300

    refactor menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,13 @@ menuItems.addEventListener('click', openEducation)
 function openMenu() {
   menu.classList.add('menu_open');
   buttonMenu.style.display = 'none';
+  buttonClose.classList.add('header__close_open')
 }
 
 function closeMenu() {
   menu.classList.remove('menu_open');
   buttonMenu.style.display = 'flex';
+  buttonClose.classList.remove('header__close_open')
 }
 
 function openEducation(e) {
