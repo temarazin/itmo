26307b127e82cf1f571029e1e8a2958aa58e6d31
Lekozhe <v.a.kozh@ya.ru>
2022-04-12 16:18:06 +0300

    fix menu modificator

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,13 +18,13 @@ menuItems.addEventListener('click', openSubmenu);
 buttonSharePublication.addEventListener('click', openPublicationPopup);
 
 function openMenu() {
-  menu.classList.add('menu_open');
+  menu.classList.add('header__menu_open');
   buttonMenu.style.display = 'none';
   buttonClose.classList.add('header__close_open');
 }
 
 function closeMenu() {
-  menu.classList.remove('menu_open');
+  menu.classList.remove('header__menu_open');
   buttonMenu.style.display = 'flex';
   buttonClose.classList.remove('header__close_open');
 }
